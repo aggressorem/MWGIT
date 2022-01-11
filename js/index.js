3315f15f2f30ee6144275d49bdfdce4d567955f6
Fedir <53612488+aggressorem@users.noreply.github.com>
2022-01-11 06:41:07 +0200

    Home work 1

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const lengthRoom1 = 8
+const widthRoom1 = 4
+const squareRoom1 = lengthRoom1 * widthRoom1
+
+
+const lengthRoom2 = 10
+const widthRoom2 = 5
+const squareRoom2 = lengthRoom2 * widthRoom2
+
+const squareFlat = squareRoom1 + squareRoom2
+
+alert(squareFlat)
